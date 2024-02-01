c642d914455062dc84c7d160a8683a5898b5d473
dubey1akash <akakakaksky@gmail.com>
2024-02-02 00:02:21 +0530

    maximum alternating bottom up

--- Maximum Alternating Subsequence Sum.cpp
+++ Maximum Alternating Subsequence Sum.cpp
@@ -34,3 +34,25 @@ public:
         return solve(0, nums, true);
     }
 };
+
+//what a depth of state knowledge
+
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<long>> t(n+1 , vector<long>(2, 0));
+         
+        for(int i=1; i<n+1; i++){
+
+            //even length ka subsequ
+            t[i][0] = max(t[i-1][1]-nums[i-1], t[i-1][0] );
+
+            t[i][1] = max(t[i-1][0]+nums[i-1], t[i-1][1] );
+        }
+
+        return max(t[n][0], t[n][1]);
+
+    }
+};
