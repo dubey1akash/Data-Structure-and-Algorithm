3ea8464c0050a6a4cc7917c6874c5dc12d8fcefb
dubey1akash <akakakaksky@gmail.com>
2024-02-24 20:32:47 +0530

    minimum depth of binary tree

---
+++ Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        if(!root->left && !root->right)
+            return 1;
+        
+        queue<TreeNode*> que;
+        que.push(root);
+        
+        int depth = 1;
+        
+        while(!que.empty()) {
+            
+            int n = que.size();
+            
+            while(n--) {
+                TreeNode* temp = que.front();
+                que.pop();
+                
+                if(!temp->left && !temp->right)
+                    return depth;
+                
+                if(temp->left)
+                    que.push(temp->left);
+                if(temp->right)
+                    que.push(temp->right);
+            }
+            
+            depth++;
+            
+        }
+        
+        return -1;
+    }
+};
+
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        if(root->left == NULL && root->right == NULL)
+            return 1;
+        
+        int left  = root->left?minDepth(root->left):INT_MAX;
+        int right = root->right?minDepth(root->right):INT_MAX;
+        
+        return 1 + min(left, right);
+    }
+};
