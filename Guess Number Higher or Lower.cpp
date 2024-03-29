4d3d088ba7242211a2bfe8e74f9fb03fe7a4e588
dubey1akash <akakakaksky@gmail.com>
2024-03-29 11:18:31 +0530

    guess number higher or lower

---
+++ Guess Number Higher or Lower.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1, r = n;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            int val = guess(mid);
+            
+            if(val == -1) {
+                r = mid-1;
+            } else if(val == 1) {
+                l = mid+1;
+            } else {
+                return mid;
+            }
+        }
+        
+        return -1;
+    }
+};
