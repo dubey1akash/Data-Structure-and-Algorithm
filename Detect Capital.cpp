cc046ba04f264351d41da2144b991da05635ff1b
dubey1akash <akakakaksky@gmail.com>
2024-04-20 11:57:38 +0530

    detect capital

---
+++ Detect Capital.cpp
@@ -0,0 +1,49 @@
+
+class Solution {
+public:
+    bool allCapitals(string word) {
+        for(char ch:word) {
+            if(ch <'A' || ch >'Z')
+                return false;
+        }
+        return true;
+    }
+    bool allSmall(string word) {
+        for(char ch:word) {
+            if(ch <'a' || ch >'z')
+                return false;
+        }
+        return true;
+    }
+    
+    bool detectCapitalUse(string word) {
+        if(allCapitals(word) || allSmall(word) || allSmall(word.substr(1)))
+            return true;
+        return false;
+    }
+};
+
+
+//Approach-2 (Small and simple)
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int countCapitals = 0;
+        
+        for(char &ch : word) {
+            if(isupper(ch))
+                countCapitals++;
+        }
+        
+        if(countCapitals == 0)
+            return true;
+        
+        if(countCapitals == word.length())
+            return true;
+        
+        if(countCapitals == 1 && isupper(word[0]))
+            return true;
+        
+        return false;
+    }
+};
